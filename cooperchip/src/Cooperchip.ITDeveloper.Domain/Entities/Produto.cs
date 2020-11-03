164eecc54668bb262750c6d3e61ca21d68a657b4
PedroReis99 <pmreis6@gmail.com>
2020-11-03 12:20:18 -0300

    Varsão 4
    ViewModels

---
+++ cooperchip/src/Cooperchip.ITDeveloper.Domain/Entities/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cooperchip.ITDeveloper.Domain.Entities
+{
+    public class Produto
+    {
+        public Produto()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Key]
+        [Display(Name = "Id do Produto")]
+        public Guid Id { get; set; }
+        
+        [Display(Name = "Nome do Produto")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [StringLength(80, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres!", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório!")]
+        [Range(0, 4000, ErrorMessage = "O campo {0} deve valer entre {2} e {1}")]
+        public decimal Valor { get; set; }
+
+
+        public int Estoque { get; set; } = 0;
+
+        [Display(Name = "Data de Validade!", Description = "Selecione uma data futura para a validade")]
+        [DataType(DataType.Date, ErrorMessage = "Data invalida!"), 
+            DisplayFormat(DataFormatString = "0:dd/MM/yyyy HH:mm")]
+        public DateTime Validade { get; set; }
+        public bool TemEmEstoque { get; set; } = true;
+    }
+}
---
+++ cooperchip/src/Cooperchip.ITDeveloper.Mvc/ViewModels/CarrinhoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Cooperchip.ITDeveloper.Domain.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cooperchip.ITDeveloper.Mvc.ViewModels
+{
+    public class CarrinhoViewModel
+    {
+        public IList<Produto> Produtos { get; set; }
+
+        [Required]
+        [Range(100, 400, ErrorMessage = "O campo {0} deve estar entre {2} e {1}")]
+        public decimal TotalCarrinho { get; set; }
+
+        [Required]
+        [StringLength(80, ErrorMessage = "O campo deve estar entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Mensagem { get; set; }
+    }
+}
