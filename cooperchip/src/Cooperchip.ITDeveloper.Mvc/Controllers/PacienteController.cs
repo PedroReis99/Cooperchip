773093fb33f243e3fb5101fb15dcce9ed44e7423
PedroReis99 <pmreis6@gmail.com>
2020-11-04 10:07:24 -0300

    ViewPacientes
    Atualização do projeto adicionando a Migration de pacientes e sua controler/views

---
+++ cooperchip/src/Cooperchip.ITDeveloper.Mvc/Controllers/PacienteController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cooperchip.ITDeveloper.Data.ORM;
+using Cooperchip.ITDeveloper.Domain.Models;
+
+namespace Cooperchip.ITDeveloper.Mvc.Controllers
+{
+    public class PacienteController : Controller
+    {
+        private readonly ITDeveloperDbContext _context;
+
+        public PacienteController(ITDeveloperDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Paciente
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Paciente.ToListAsync());
+        }
+
+        // GET: Paciente/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+        }
+
+        // GET: Paciente/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                //paciente.Id = Guid.NewGuid();Não Usar
+                _context.Add(paciente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+                //return RedirectToAction(Index); Funciona da mesma forma que o metodo de cima
+            }
+            return View(paciente);
+        }
+
+        //GET
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente.FindAsync(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Paciente/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Paciente paciente)
+        {
+            if (id != paciente.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(paciente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PacienteExists(paciente.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(paciente);
+        }
+
+        // GET: Paciente/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+        }
+
+        // POST: Paciente/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var paciente = await _context.Paciente.FindAsync(id);
+            _context.Paciente.Remove(paciente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PacienteExists(Guid id)
+        {
+            return _context.Paciente.Any(e => e.Id == id);
+        }
+    }
+}
