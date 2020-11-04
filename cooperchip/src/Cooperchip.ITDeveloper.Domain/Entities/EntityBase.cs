08feebae59960f3c1cf48f3dac104cb5418d7c77
PedroReis99 <pmreis6@gmail.com>
2020-11-04 16:15:20 -0300

    Adição do estado do paciente ao banco de dados

---
+++ cooperchip/src/Cooperchip.ITDeveloper.Domain/Entities/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cooperchip.ITDeveloper.Domain.Entities
+{
+    public class EntityBase
+    {
+        public EntityBase()
+        {
+            this.Id = Guid.NewGuid();//Gera o Guid aleatório
+        }
+
+        public Guid Id { get; set; }
+    }
+}
